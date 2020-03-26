f8b014d645f2e169d62af1f0eb7326f5940d433e
Ryan.Wang <ranwang1995@outlook.com>
2020-03-27 02:33:18 +0800

    [WangRan] fix: make instance variables private

--- src/main/java/com/thoughtworks/DatabaseUtil.java
+++ src/main/java/com/thoughtworks/DatabaseUtil.java
@@ -5,9 +5,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseUtil {
-    public static final String URL = "jdbc:mysql://localhost:3306/demo?useUnicode=true&characterEncoding=utf-8&serverTimezone=Hongkong";
-    public static final String USER = "root";
-    public static final String PASSWORD = "password";
+    private static final String URL = "jdbc:mysql://localhost:3306/demo?useUnicode=true&characterEncoding=utf-8&serverTimezone=Hongkong";
+    private static final String USER = "root";
+    private static final String PASSWORD = "password";
     private static Connection connection = null;
 
     static{
